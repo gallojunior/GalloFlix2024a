6203defb339699adc65d6437c6af6453f297b942
GalloJunior <gallojunior@gmail.com>
2024-04-10 11:36:13 -0300

    Incluido os pacotes de conexão com banco e iniciada a classe de contexto

---
+++ GalloFlix/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using GalloFlix.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GalloFlix.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<MovieGenre> MovieGenres { get; set; }
+}
---
+++ GalloFlix/Models/MovieGenre.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GalloFlix.Models;
+
+[Table("MovieGenre")]
+public class MovieGenre
+{
+    [Key, Column(Order = 1)]
+    public int MovieId { get; set; }
+    [ForeignKey("MovieId")]
+    public Movie Movie { get; set; }
+
+    [Key, Column(Order = 2)]
+    public sbyte GenreId { get; set; }
+    [ForeignKey("GenreId")]
+    public Genre Genre { get; set; }
+}
