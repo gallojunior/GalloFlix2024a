6842ab792892b4224143bcdca71d3b72d084bfff
GalloJunior <gallojunior@gmail.com>
2024-05-20 20:52:52 -0300

    Criação do AccountController e página de login

---
+++ GalloFlix/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+using System.Net.Mail;
+using GalloFlix.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GalloFlix.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager
+    )
+    {
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
+    {
+        LoginVM loginVM = new()
+        {
+            ReturnUrl = returnUrl ?? Url.Content("~/")
+        };
+        return View(loginVM);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            
+        }
+        return View(login);
+    }
+
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
---
+++ GalloFlix/ViewModels/LoginVM.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GalloFlix.ViewModels;
+
+public class LoginVM
+{
+    [Display(Name = "Email ou Nome de Usuário")]
+    [Required(ErrorMessage = "Por favor, informe seu email ou nome de usuário")]
+    public string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Senha de Acesso")]
+    [Required(ErrorMessage = "Por favor, informe sua senha")]
+    public string Password { get; set; }
+
+    [Display(Name = "Manter Conectado?")]
+    public bool RememberMe { get; set; } = false;
+    
+    public string ReturnUrl { get; set; }
+}
